bcf295bfab4c837db885a17e41472bbabba678af
Fabrício K <108683940+Fabken42@users.noreply.github.com>
2024-03-06 18:26:02 -0300

    Update ProgressBar.js

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -8,40 +8,40 @@ export default function BarraProgresso() {
             <Row className='text-start'>
                 <Col md={4} className="mb-3">
                     <h5>HTML</h5>
-                    <ProgressBar className='barra-progresso' now={85} label="85%" variant="success" />
+                    <ProgressBar className='barra-progresso' now={90} label="90%" variant="success" />
                 </Col>
                 <Col md={4} className="mb-3">
                     <h5>CSS</h5>
-                    <ProgressBar className='barra-progresso' now={80} label="80%" variant="success" />
+                    <ProgressBar className='barra-progresso' now={85} label="85%" variant="success" />
                 </Col>
                 <Col md={4} className="mb-3">
                     <h5>JavaScript</h5>
-                    <ProgressBar className='barra-progresso' now={60} label="60%" variant="warning" />
+                    <ProgressBar className='barra-progresso' now={75} label="75%" variant="success" />
                 </Col>
             </Row>
             <Row className="mt-3 text-start">
                 <Col md={4} className="mb-3">
-                    <h5>React</h5>
-                    <ProgressBar className='barra-progresso' now={75} label="75%" variant="success" />
+                    <h5>React.js</h5>
+                    <ProgressBar className='barra-progresso' now={85} label="85%" variant="success" />
                 </Col>
                 <Col md={4} className="mb-3">
-                    <h5>Bootstrap</h5>
+                    <h5>Node.js</h5>
                     <ProgressBar className='barra-progresso' now={55} label="55%" variant="warning" />
                 </Col>
                 <Col md={4} className="mb-3">
-                    <h5>Firebase</h5>
-                    <ProgressBar className='barra-progresso' now={40} label="40%" variant="danger" />
+                    <h5>Python</h5>
+                    <ProgressBar className='barra-progresso' now={55} label="55%" variant="warning" />
                 </Col>
             </Row>
             <h3 className='text-center mb-3 border-top pt-3'>Idiomas</h3>
             <Row className="mt-3 text-start">
                 <Col md={4} className="mb-3">
                     <h5>Inglês</h5>
-                    <ProgressBar className='barra-progresso' now={80} label="80%" variant="success" />
+                    <ProgressBar className='barra-progresso' now={85} label="85%" variant="success" />
                 </Col>
                 <Col md={4} className="mb-3">
                     <h5>Espanhol</h5>
-                    <ProgressBar className='barra-progresso' now={50} label="50%" variant="warning" />
+                    <ProgressBar className='barra-progresso' now={40} label="40%" variant="danger" />
                 </Col>
                 <Col md={4} className="mb-3">
                     <h5>Japonês</h5>
