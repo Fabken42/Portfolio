2f8f4a611100939ce911d16370665aeeb2519634
Fabrício K <108683940+Fabken42@users.noreply.github.com>
2024-03-06 18:04:09 -0300

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Carousel } from 'react-bootstrap';
 import siteAimTrainer from '../img/siteAimTrainer.png'
-import siteJogoVelha from '../img/siteJogoVelha.png'
 import siteListaTarefas from '../img/siteListaTarefas.png'
-import siteOqueVocePrefere from '../img/siteOqueVocePrefere.png'
 import sitePrevisaoTempo from '../img/sitePrevisaoTempo.png'
 import siteSimonGame from '../img/siteSimonGame.png'
 
@@ -12,11 +10,6 @@ const items = [
         imageUrl: siteListaTarefas,
         link: 'https://gerenciador-de-tarefas-seven.vercel.app/',
         titulo: 'Gerenciador de Tarefas',
-    },
-    {
-        imageUrl: siteJogoVelha,
-        link: 'https://jogo-velha-multiplayer.web.app/',
-        titulo: 'Jogo da Velha Multiplayer',
     },
     {
         imageUrl: siteSimonGame,
@@ -32,12 +25,7 @@ const items = [
         imageUrl: siteAimTrainer,
         link: 'https://aim-trainer-fawn.vercel.app/',
         titulo: 'Acerte os alvos!',
-    },
-    {
-        imageUrl: siteOqueVocePrefere,
-        link: 'https://oquevoceprefere-e2799.web.app/',
-        titulo: 'O que você prefere?',
-    },
+    }
 ];
 
 export default function Projects() {
